7b55ab988a2cbe96de6ca2d4ef3c23efea1468ee
Shun Furusawa <furu.shun1113@gmail.com>
2023-01-22 23:47:08 +0900

    並列化

--- src/OpenMP_test.cpp
+++ src/OpenMP_test.cpp
@@ -1,6 +1,8 @@
 ﻿#pragma warning(disable: 4996)
 #include <iostream>// 時間計測用：気にしないこと
 #include <chrono>// 時間計測用：気にしないこと
+#include "stdafx.h"
+#include <omp.h>
 
 // 画像処理ライブラリの導入
 #define STB_IMAGE_IMPLEMENTATION
@@ -14,6 +16,7 @@ using namespace std::chrono;// 時間計測用：気にしないこと
 // モノクロ化
 bool monochrome(const char* filename)
 {
+
     int width, height; //画像サイズの格納先
     int bpp; //一画素のバイト数
     unsigned char* pixels = stbi_load(filename, &width, &height, &bpp, 0);
@@ -23,18 +26,27 @@ bool monochrome(const char* filename)
     auto start = system_clock::now();// 時間計測用：気にしないこと
 
     // ■ OpenMPを使って並列化してください。
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            unsigned char* r = &pixels[(y * width + x) * bpp + 0];
-            unsigned char* g = &pixels[(y * width + x) * bpp + 1];
-            unsigned char* b = &pixels[(y * width + x) * bpp + 2];
 
-            int lum = ((int)*r + (int)*g + (int)*b) / 3;
-            *r = lum;
-            *g = lum;
-            *b = lum;
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+
+                unsigned char* r = &pixels[(y * width + x) * bpp + 0];
+                unsigned char* g = &pixels[(y * width + x) * bpp + 1];
+                unsigned char* b = &pixels[(y * width + x) * bpp + 2];
+
+                int lum = ((int)*r + (int)*g + (int)*b) / 3;
+                *r = lum;
+                *g = lum;
+                *b = lum;
+            }
         }
     }
+
 
     // 時間計測用：気にしないこと
     auto end = system_clock::now();
@@ -61,52 +73,78 @@ bool blur(const char *filename, int num)
 
     // ■ OpenMPを使って並列化してください。
     // 依存性があり、並列化すると処理の順番によって結果が変わる可能性があるので、変わらないように注意すること
-    for (int i = 0; i < num; i++) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                unsigned char* r = &pixels[(y * width + x) * bpp + 0];
-                unsigned char* g = &pixels[(y * width + x) * bpp + 1];
-                unsigned char* b = &pixels[(y * width + x) * bpp + 2];
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int i = 0; i < num; i++)
+        {
+            for (int y = 0; y < height; y++) 
+            {
+                for (int x = 0; x < width; x++) 
+                {
+                    unsigned char* r = &pixels[(y * width + x) * bpp + 0];
+                    unsigned char* g = &pixels[(y * width + x) * bpp + 1];
+                    unsigned char* b = &pixels[(y * width + x) * bpp + 2];
 
-                int cr = *r;
-                int cg = *g;
-                int cb = *b;
-                int pixel_count = 1;
-                // 左の色を加える
-                if (0 < x) {
-                    cr += *(r - bpp);
-                    cg += *(g - bpp);
-                    cb += *(b - bpp);
-                    pixel_count++;
+                    int cr = *r;
+                    int cg = *g;
+                    int cb = *b;
+                    int pixel_count = 1;
+                   
+                        #pragma omp section
+                        {
+                            // 左の色を加える
+                            if (0 < x) {
+                                cr += *(r - bpp);
+                                cg += *(g - bpp);
+                                cb += *(b - bpp);
+                                pixel_count++;
+                            }
+                        }
+                        #pragma omp section
+                        {
+                            // 右の色を加える
+                            if (x < width - 1) {
+                                cr += *(r + bpp);
+                                cg += *(g + bpp);
+                                cb += *(b + bpp);
+                                pixel_count++;
+                            }
+                        }
+                        #pragma omp section
+                        {
+                            // 上の色を加える
+                            if (0 < y) 
+                            {
+                                cr += *(r - width * bpp);
+                                cg += *(g - width * bpp);
+                                cb += *(b - width * bpp);
+                                pixel_count++;
+                            }
+                        }
+                       
+                        #pragma omp section
+                        {
+                            // 下の色を加える
+                            if (y < height - 1) 
+                            {
+                                cr += *(r + width * bpp);
+                                cg += *(g + width * bpp);
+                                cb += *(b + width * bpp);
+                                pixel_count++;
+                            }
+                        }
+                     
+
+                    // そのまま平均をとると桁落ちで暗くなるので、0.5だけ明るくする
+                    *r = (cr + pixel_count / 2) / pixel_count;
+                    *g = (cg + pixel_count / 2) / pixel_count;
+                    *b = (cb + pixel_count / 2) / pixel_count;
                 }
-                // 右の色を加える
-                if (x < width - 1) {
-                    cr += *(r + bpp);
-                    cg += *(g + bpp);
-                    cb += *(b + bpp);
-                    pixel_count++;
-                }
-                // 上の色を加える
-                if (0 < y) {
-                    cr += *(r - width * bpp);
-                    cg += *(g - width * bpp);
-                    cb += *(b - width * bpp);
-                    pixel_count++;
-                }
-                // 下の色を加える
-                if (y < height - 1) {
-                    cr += *(r + width * bpp);
-                    cg += *(g + width * bpp);
-                    cb += *(b + width * bpp);
-                    pixel_count++;
-                }
-                // そのまま平均をとると桁落ちで暗くなるので、0.5だけ明るくする
-                *r = (cr + pixel_count / 2) / pixel_count;
-                *g = (cg + pixel_count / 2) / pixel_count;
-                *b = (cb + pixel_count / 2) / pixel_count;
             }
         }
     }
+
 
     // 時間計測用：気にしないこと
     auto end = system_clock::now();
